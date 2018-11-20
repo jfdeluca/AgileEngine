bf7b9caa46b103be60a9b24046848d1ebe09b810
panchopure <30704190+panchopure@users.noreply.github.com>
2018-11-20 17:09:12 -0300

    stuff

--- AgileEngine/src/com/AgileEngine/Main.scala
+++ AgileEngine/src/com/AgileEngine/Main.scala
@@ -10,7 +10,7 @@ import scala.util.{Failure, Success, Try}
 import scala.collection.JavaConversions._
 import scala.swing.{Button, FileChooser}
 
-object Main extends App{
+object Main extends App {
 
 
   private val CHARSET_NAME = "utf8"
@@ -26,12 +26,25 @@ object Main extends App{
   val targetElementId: String = "make-everything-ok-button"
   val cssQuery = "[class*=\"btn btn-success\"]"
   val href = "#check-and-ok"
+  val title = "[title=\"Make-Button\"]"
 
 
-
-  //In the case that the file has not been found by its ID name it will be searched by css Class btn-success
+  //In the case that the file has not been found by its ID name it will be searched by class and Title
   //Sorry for printing and not logging, been having some issues in this pc :S
-  getAll()
+
+  findElementById(new File(resourcePath), targetElementId).map { button =>
+    button.attributes().asList().map(attr => s"${attr.getKey}=${attr.getValue}").mkString(", ")
+  } match {
+    case Success(attrs) => println("Found by Id at Target element attrs: [{}]", attrs)
+    case Failure(ex) => {
+      println("Not Found by Id")
+      getAllByTitle()
+      getAllByClass()
+
+    }
+
+  }
+
 
 
   def findElementById(htmlFile: File, targetElementId: String): Try[Element] = Try {
@@ -39,37 +52,43 @@ object Main extends App{
   }.map(_.getElementById(targetElementId))
 
 
-
   def findElementsByQuery(htmlFile: File, cssQuery: String): Try[Elements] = Try {
     Jsoup.parse(htmlFile, CHARSET_NAME, htmlFile.getAbsolutePath)
   }.map(_.select(cssQuery))
 
+  def findElementsByTitle(htmlFile: File, cssQuery: String): Try[Elements] = Try {
+    Jsoup.parse(htmlFile, CHARSET_NAME, htmlFile.getAbsolutePath)
+  }.map(_.select(title))
 
-  def getAll() = {
-    findElementById(new File(resourcePath), targetElementId).map { button =>
-      button.attributes().asList().map(attr => s"${attr.getKey}=${attr.getValue}").mkString(", ")
-    } match {
-      case Success(attrs) => println("Target element attrs: [{}]", attrs)
-      case Failure(ex) => {
-        println("Not Found by Id")
-        getAllByClass()
+
+  def getAllByTitle(): Unit = {
+    findElementsByTitle(new File(resourcePath), cssQuery).map { buttons =>
+      buttons.iterator().toList.map { button =>
+        button.attributes().toList.map(attr => s"${attr.getKey}=${attr.getValue}").mkString(", ")
       }
+    } match {
+      case Success(attrsList) => attrsList.foreach { attrs => println("Found by Title at Target element attrs: [{}]", attrs) }
+      case Failure(ex) => println("Button not Found", ex)
     }
+
+
   }
 
-  def getAllByClass()= {
+
+
+  def getAllByClass(): Unit = {
     findElementsByQuery(new File(resourcePath), cssQuery).map { buttons =>
       buttons.iterator().toList.map { button =>
         button.attributes().toList.map(attr => s"${attr.getKey}=${attr.getValue}").mkString(", ")
       }
     } match {
-      case Success(attrsList) => attrsList.foreach { attrs => println("Target element attrs: [{}]", attrs) }
-      case Failure(ex) => println("Error occurred.", ex)
+      case Success(attrsList) => attrsList.foreach { attrs => println("Found by class at Target element attrs: [{}]", attrs) }
+      case Failure(ex) => println("Not Found By Class", ex)
+
     }
 
+
   }
-
-
 
 }
 
